25dcc5c6a25b650449cbc18c8b2fc7c6f6ea08b9
Daniil <danechka.bo@yandex.ru>
2023-03-22 16:13:48 +0300

    dots for slider

--- js/script.js
+++ js/script.js
@@ -284,13 +284,16 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     // slider
-    const sliderWrapper = document.querySelector('.offer__slider-wrapper'),
+    const slider = document.querySelector('.offer__slider'),
+        sliderWrapper = slider.querySelector('.offer__slider-wrapper'),
         slides = sliderWrapper.querySelectorAll('.offer__slide'),
-        sliderPrevBtn = document.querySelector('.offer__slider-prev'),
-        sliderNextBtn = document.querySelector('.offer__slider-next'),
-        curentSlide = document.querySelector('#current'),
-        totalSlides = document.querySelector('#total'),
+        sliderPrevBtn = slider.querySelector('.offer__slider-prev'),
+        sliderNextBtn = slider.querySelector('.offer__slider-next'),
+        curentSlide = slider.querySelector('#current'),
+        totalSlides = slider.querySelector('#total'),
         slidrLine = sliderWrapper.querySelector('.slider__line'),
+        sliderDotsWrapper = document.createElement('ol'),
+        dotsArray = [],
         wrapperWidth = window.getComputedStyle(sliderWrapper).width;
     let slideIndex = 1,
         offset = 0;
@@ -305,6 +308,38 @@ window.addEventListener('DOMContentLoaded', () => {
         slide.style.width = wrapperWidth;
     })
 
+    // размещение обертки точек на слайдере и добавление класса
+    slider.style.position = 'relative';
+    sliderDotsWrapper.classList.add('carousel-indicators');
+    slider.append(sliderDotsWrapper);
+
+    // создание точек, размещение точек в обертке, добавление точкам класса и атрибута для переключения, добавления точек в массив
+    for (let i = 0; i < slides.length; i++) {
+        const sliderDot = document.createElement('li');
+        sliderDot.setAttribute('data-slide-to', i + 1);
+        sliderDot.classList.add('dot');
+
+        sliderDotsWrapper.append(sliderDot);
+        dotsArray.push(sliderDot);
+        if (i == 0) {
+            changeDot(i);
+        }
+    }
+
+    // смена цвета у точки (активность точки)
+    function changeDot(i) {
+        dotsArray.forEach(dot => {
+            dot.style.cssText = `
+            background-color: #fff;
+            opacity: 0.5;
+            `;
+            dotsArray[i].style.cssText = `
+            background-color: grey;
+            opacity: 1;
+            `;
+        })
+    }
+
     // переключение слайдов вперед (slider line двигается влево на ширину слайда)
     sliderNextBtn.addEventListener('click', () => {
         if (offset == +wrapperWidth.slice(0, wrapperWidth.length - 2) * (slides.length - 1)) {
@@ -316,6 +351,8 @@ window.addEventListener('DOMContentLoaded', () => {
         }
         curentSlide.textContent = getZero(slideIndex);
         slidrLine.style.transform = `translateX(-${offset}px)`;
+
+        changeDot(slideIndex - 1);
     })
 
     // переключение слайда назад (двигается вправо)
@@ -329,9 +366,23 @@ window.addEventListener('DOMContentLoaded', () => {
         }
         curentSlide.textContent = getZero(slideIndex);
         slidrLine.style.transform = `translateX(-${offset}px)`;
+
+        changeDot(slideIndex - 1);
     })
 
+    // переключение слайдов по нажитю на точки
+    dotsArray.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
 
+            slideIndex = slideTo;
+            offset = +wrapperWidth.slice(0, wrapperWidth.length - 2) * (slideTo - 1);
+            slidrLine.style.transform = `translateX(-${offset}px)`;
+            curentSlide.textContent = getZero(slideIndex);
+            changeDot(slideIndex - 1);
+
+        })
+    })
 
 
 
